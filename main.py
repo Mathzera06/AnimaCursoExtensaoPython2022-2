19975441a6a303cd8d215fba6860cd8750a9dbae
mathzera <102606784+Mathzera06@users.noreply.github.com>
2022-10-20 01:16:30 +0000

    .format idade e nome

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ idade = 18
 #print(nome, idade)
 #print("Minha idade é " +str (idade))
 #print (f"Minha idade é {idade}\n")
-print ("Minha idade é {}".format(idade))
+#print ("Minha idade é {}".format(idade))
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
