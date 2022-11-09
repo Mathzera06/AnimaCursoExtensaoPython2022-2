e5a3dd3f6b8c0ba60fdf6c5cbcd7bfa6c3f1b045
mathzera <102606784+Mathzera06@users.noreply.github.com>
2022-11-09 23:23:36 +0000

    exemplo while

---
+++ aula3-09.py
@@ -0,0 +1,23 @@
+#print("hello world")
+#contador = contador+1
+contador = 1
+
+while(contador <= 6):
+  print (contador)
+  contador = contador + 1
+
+fruta = "morango"
+print(fruta)
+
+frutas = ["morango", "laranja", "manga"]
+#print (frutas[2])
+
+#print(len(frutas))
+frutas.append("banana")
+print(len(frutas))
+print (frutas)
+
+i= 0 
+while (i < 4):
+  print (frutas[i])
+  i = i + 1
